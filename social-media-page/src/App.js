c8ed17d4f7c6d023c135bcd07b93c14977c6dea0
kallurisandeepkumar <sandeepkalluri004@gmail.com>
2024-08-06 11:38:26 +0530

    added spelling mistake for hashrouter

--- social-media-page/src/App.js
+++ social-media-page/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { ashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PostFeed from './components/PostFeed';
 import PostDetail from './components/PostDetail';
 import './App.css';
